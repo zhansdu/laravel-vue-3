b019d07b0822dd4cd6cb5612cebb9f0b02d6b070
zhan <zhanzhaksilik@gmail.com>
2021-09-28 10:55:42 +0600

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
 
 
-    </head>
-    <body>
+</head>
 
-        <div  id="app">
+<body>
 
-        </div>
-        </script>
-        <script src="{{asset('js/app.js')}}"></script>
-    </body>
+    <div id="app">
+
+    </div>
+    </script>
+    <script src="{{asset('js/main.js')}}"></script>
+</body>
+
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js').vue();
+mix
+    .ts('resources/js/main.ts', 'public/js')
+    .vue({ version: 3 });
 mix.sass('resources/sass/app.scss', 'public/css');
